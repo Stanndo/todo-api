4bc6664fbaf3baa8756435043d7a9c0b826030ea
Slav <stan458@abv.bg>
2022-12-26 14:55:29 +0200

    finishing details

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,11 +1,7 @@
 function enableCors(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // * means we allow all domains to have access to our api
-  // if we want this api to be used only from us then we can specify that instead of * all
-
+ 
   res.setHeader("Access-Control-Allow-Methods","GET,POST,PATCH,DELETE,OPTIONS");
-  // OPTIONS is not a request that we will send by ourselves, but it is automaticly send request by the browser
-  // when we use ajax request
 
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
